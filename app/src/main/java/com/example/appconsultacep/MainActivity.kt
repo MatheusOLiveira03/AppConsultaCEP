cb1e6f94bd03f45053d6aa13884b4c79458feeac
Matheus <matheusoliveiradepaula2009@gmail.com>
2026-09-11 09:36:54 -0300

    AppConsultaCEP

---
+++ app/src/main/java/com/example/appconsultacep/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.appconsultacep
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
+import com.example.appconsultacep.api.ViaCepClient
+import kotlinx.coroutines.launch
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val edtCep = findViewById<EditText>(R.id.edtCep)
+        val btnConsultar = findViewById<Button>(R.id.btnConsultar)
+        val txtLogradouro = findViewById<EditText>(R.id.txtLogradouro)
+        val txtBairro = findViewById<EditText>(R.id.txtBairro)
+        val txtUf = findViewById<EditText>(R.id.txtUf)
+        val txtDdd = findViewById<EditText>(R.id.txtDdd)
+        val txtLocalidade = findViewById<EditText>(R.id.txtLocalidade)
+
+        btnConsultar.setOnClickListener {
+            val cep = edtCep.text.toString()
+            if (cep.length != 8) {
+                edtCep.error = "CEP inválido"
+                return@setOnClickListener
+            }
+            lifecycleScope.launch {
+                val endereco = ViaCepClient.instance.buscarEndereco(cep)
+                txtLogradouro.setText(endereco.logradouro)
+                txtBairro.setText(endereco.bairro)
+                txtUf.setText(endereco.uf)
+                txtDdd.setText(endereco.ddd)
+                txtLocalidade.setText(endereco.localidade)
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/appconsultacep/model/ResponseEndereco.kt
@@ -0,0 +1,10 @@
+package com.example.appconsultacep.model
+
+data class ResponseEndereco(
+    val logradouro : String,
+    val bairro : String,
+    val localidade : String,
+    val uf : String,
+    val ddd : String
+)
+
